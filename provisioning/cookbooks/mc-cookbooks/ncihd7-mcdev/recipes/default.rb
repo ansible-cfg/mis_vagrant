05fd839b71a33287e56c7020cec692d07f75ebbe
grndlvl <grndlvl@103553.no-reply.drupal.org>
2013-07-01 00:47:04 -0400

    Adding vhost for site

--- provisioning/cookbooks/mc-cookbooks/ncihd7-mcdev/recipes/default.rb
+++ provisioning/cookbooks/mc-cookbooks/ncihd7-mcdev/recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: hi_ncihd7_mcdev
+# Cookbook Name:: ncihd7-mcdev
 # Recipe:: default
 #
 # Copyright 2013, YOUR_COMPANY_NAME
@@ -7,8 +7,20 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# create a mysql database for homeimprovement
+# Create a mysql database for homeimprovement.
 mysql_database 'hi_ncihd7_mcdev' do
-  connection ({:host => "localhost", :username => 'root', :password => node['mysql']['server_root_password']})
+  connection ({
+    :host => "localhost",
+    :username => 'root',
+    :password => node['mysql']['server_root_password']
+  })
   action :create
+end
+
+# Create virtual host and enable site.
+web_app node[:domain] do
+  allow_override "All"
+  docroot node[:docroot]
+  server_aliases []
+  server_name node[:domain]
 end
