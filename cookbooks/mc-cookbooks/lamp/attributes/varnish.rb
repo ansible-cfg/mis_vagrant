adcc0dcd676391fbd7f2e0805a8d092f3d71c9b9
grndlvl <grndlvl@103553.no-reply.drupal.org>
2013-11-13 08:20:18 -0500

    Adding configurable settings for varnish

--- cookbooks/mc-cookbooks/lamp/attributes/varnish.rb
+++ cookbooks/mc-cookbooks/lamp/attributes/varnish.rb
@@ -10,4 +10,8 @@ include_attribute "varnish"
 
 default['varnish']['version'] = '3.0'
 default['varnish']['vcl_source'] = "varnish-3.erb"
-default['varnish']['vcl_cookbook'] = "lamp"
+default['varnish']['vcl_cookbook'] = 'lamp'
+
+default['varnish']['backend_host'] = '127.0.0.1'
+default['varnish']['backend_port'] = '80'
+default['varnish']['secret_file'] = ''
