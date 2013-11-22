0d12b76f973b82da8edd4fefb122df66ac563c1e
grndlvl <grndlvl@103553.no-reply.drupal.org>
2013-11-22 14:44:02 -0500

    Adding some configurable attributes for apc

--- cookbooks/mc-cookbooks/lamp/attributes/default.rb
+++ cookbooks/mc-cookbooks/lamp/attributes/default.rb
@@ -28,3 +28,11 @@ default['mysql']['tunable']['query_cache_size'] = "64M"
 # Custom PHP settings.
 default['lamp']['php']['apache_conf_dir'] = '/etc/php5/apache2'
 default['lamp']['php']['error_reporting'] = 'E_ALL'
+
+# APC settings.
+default['lamp']['apc']['enabled'] = 1
+default['lamp']['apc']['max_file_size'] = "2M"
+default['lamp']['apc']['shm_segments'] = 1
+default['lamp']['apc']['shm_size'] = "128M"
+default['lamp']['apc']['stat'] = 0
+default['lamp']['apc']['ttl'] = 14400
