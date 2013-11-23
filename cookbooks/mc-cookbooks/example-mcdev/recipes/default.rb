48bea1a3a025df651b730596dfdca618a5f9237f
grndlvl <grndlvl@103553.no-reply.drupal.org>
2013-11-23 12:42:06 -0500

    Fist pass at creating post setup scripts and invoker

--- cookbooks/mc-cookbooks/example-mcdev/recipes/default.rb
+++ cookbooks/mc-cookbooks/example-mcdev/recipes/default.rb
@@ -31,4 +31,70 @@ web_app node[:domain] do
   server_name node[:domain]
 end
 
+###############################################################################
+# Post post installation/up scripts.
+#
+# This should almost always be the last thing you want to run.
+#
+# - post-installation.sh: Runs only ever once.
+# - post-up.sh: Runs every time the provision scripts are ran.
+#
+# @todo: Do we need any other hooks?
+# @todo: Should we move this into its own recipe or make this into a LWRP?
+###############################################################################
 
+# Determine if we have already run installation.
+installed_tracker = "/home/vagrant/.mis_vagrant.installed"
+is_installed = ::File.exists?(installed_tracker)
+
+# The config file output.
+config_file = "/tmp/mis_vagrant.config.json"
+
+# Script paths.
+post_install = "#{node[:docroot]}/sites/all/mis_vagrant/post-install.sh"
+post_up = "#{node[:docroot]}/sites/all/mis_vagrant/post-up.sh"
+
+# Write the configuration file.
+file config_file do
+  # @todo: Figure out a way to get the final value of the entire node object.
+  #        Because right now it splits it out into default, normal, override.
+  #        If not possible we need to add the specific variables that we think
+  #        would be most usefule.
+  content node.to_json
+  mode "0644"
+  owner "vagrant"
+  group "vagrant"
+end
+
+# Make sure the scripts are executable.
+# @todo: We probably should use resource (or maybe something else? need a little
+#        bit more research for this.)
+file post_up do
+  mode "0755"
+end
+
+# Run the installation script only if the system hasn't already been installed.
+if !is_installed
+  file post_install  do
+    mode "0755"
+  end
+
+  execute post_install do
+    command "#{post_install} #{config_file}"
+    action :run
+    cwd node[:docroot]
+  end
+
+  # Lets us know if the system install has already ran.
+  file installed_tracker  do
+    content Time.now.getutc.inspect
+    mode "0644"
+  end
+end
+
+# Run the post installation script every time.
+execute post_up do
+  command "#{post_up} #{config_file}"
+  action :run
+  cwd node[:docroot]
+end
